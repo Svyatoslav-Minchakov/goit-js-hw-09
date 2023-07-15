810cdeff1f00fc2fdd67a619492d473c7d1dbb45
Svyatoslav-Minchakov <76057514+Svyatoslav-Minchakov@users.noreply.github.com>
2023-07-15 23:45:25 +0000

    Deploying to gh-pages from @ Svyatoslav-Minchakov/goit-js-hw-09@9b6b2d2d9cfe5232110a51c93b58bbc59c6100c2 🚀

---
+++ 01-color-switcher.dfbcc1c2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){r=setInterval((function(){return n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(r)}));var r=null}();
+//# sourceMappingURL=01-color-switcher.dfbcc1c2.js.map
