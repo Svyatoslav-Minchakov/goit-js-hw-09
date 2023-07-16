a8f58e3d9db152290defba819f2d84c5fece4bee
Svyatoslav-Minchakov <76057514+Svyatoslav-Minchakov@users.noreply.github.com>
2023-07-16 12:19:05 +0000

    Deploying to gh-pages from @ Svyatoslav-Minchakov/goit-js-hw-09@224746fa0a11e7f0f7122e8c42d28d2eb76f6fd6 🚀

--- 01-color-switcher.f7bf0343.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((()=>n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3)})),e.addEventListener("click",(function(){clearInterval(o)}));let o=null;
-//# sourceMappingURL=01-color-switcher.f7bf0343.js.map
