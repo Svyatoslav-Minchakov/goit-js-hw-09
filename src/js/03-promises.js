8f994f8b504926ecfe299741a5f9599f263dbabe
Святослав Минчаков <sminchakov@gmail.com>
2023-07-20 21:21:47 +0300

    third task is finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+
+
+const pageForm = document.querySelector('.form');
+pageForm.addEventListener('submit', generatePromises);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.5;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Promise ${position} після ${delay}мс`);
+      } else {
+        reject(`Promise ${position} після ${delay}мс`);
+      }
+    }, delay);
+  });
+}
+
+function generatePromises(event) {
+  event.preventDefault();
+  const delay = Number(document.querySelector('input[name="delay"]').value);
+  const step = Number(document.querySelector('input[name="step"]').value);
+  const amount = Number(document.querySelector('input[name="amount"]').value);
+  
+  let currentPromise = 1;
+
+  const intervalId = setInterval(() => {
+    if (currentPromise > amount) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    createPromise(currentPromise, delay)
+      .then((position) => {
+        console.log(`✅ Виконано ${position} через ${step}мс`);
+      })
+      .catch((position) => {
+        console.log(`❌ Відхилено ${position} через ${step}мс`);
+      });
+
+    currentPromise += 1;
+  }, step);
 }
